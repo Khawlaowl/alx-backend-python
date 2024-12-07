f74eb5e539b1f88d60da53feeae9289486821363
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2024-12-07 10:47:43 +0100

    f

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,10 @@
+from seed import paginate_users
+
+def lazy_paginate(page_size):
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
